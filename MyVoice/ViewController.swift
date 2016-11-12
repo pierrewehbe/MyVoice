ebac2051ba12e88d15297436951e4fec6f605704
Pierre Wehbe <pew01@mail.aub.edu>
2016-11-12 21:40:05 +0200

    Initial Commit

---
+++ MyVoice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyVoice
+//
+//  Created by Pierre on 11/12/16.
+//  Copyright © 2016 Pierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
